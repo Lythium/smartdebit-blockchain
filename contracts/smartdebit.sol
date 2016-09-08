06142615549cb215c65beb09d3975064940a7651
Michel Kaporin <michel.kaporin@gmail.com>
2016-09-08 10:45:22 +0200

    Contract updated, web part added

--- contracts/smartdebit.sol
+++ contracts/smartdebit.sol
@@ -11,8 +11,20 @@ contract smartdebit {
     // Mapping from account address to its index in the address array. Used to delete.
     mapping (address => uint) private addressIndices;
     address[] private accountAddresses; // All direct debit account addresses
+    address private contractOwner; // Who owns a contract
     
-    function createDirectDebit(address beneficiary, uint fee, uint period)  {
+    function smartdebit() {
+        contractOwner = msg.sender;
+    }
+    
+    modifier isOwner() {
+        if (msg.sender != contractOwner) {
+            throw;
+        }
+        _
+    }
+    
+    function createDirectDebit(address beneficiary, uint fee, uint period) isOwner {
         if (accountsMapping[beneficiary].exists == true) {
             throw; // Account already has a direct debit setup against it
         }
@@ -20,11 +32,11 @@ contract smartdebit {
         var acc = Account(beneficiary, fee, period, true);
         accountsMapping[beneficiary] = acc;
         
-        accountAddresses.push(beneficiary);
+        accountAddresses.push(beneficiary); 
         addressIndices[beneficiary] = accountAddresses.length+1;
     }
     
-    function getDirectDebit(address beneficiary) returns (uint fee, uint period) {
+    function getDirectDebit(address beneficiary) isOwner returns (uint fee, uint period) {
         if (accountsMapping[beneficiary].exists != true) {
             throw;
         }
@@ -33,11 +45,15 @@ contract smartdebit {
         period = accountsMapping[beneficiary].period;
     }
     
-    function getAllDirectDebits() returns (address[] addresses) {
+    function getAllDirectDebits() isOwner returns (address[] addresses) {
       addresses = accountAddresses;
     }
     
-    function deleteDirectDebit(address beneficiary) {
+    function changeDirectDebit(address beneficiary, uint newAmount) isOwner {
+        accountsMapping[beneficiary].fee = newAmount;
+    }
+    
+    function deleteDirectDebit(address beneficiary) isOwner {
         if (accountsMapping[beneficiary].exists != true) {
             throw;
         }
@@ -45,5 +61,14 @@ contract smartdebit {
         delete accountsMapping[beneficiary];
         var index = addressIndices[beneficiary]; // Getting index of the beneficiary in the address array
         delete accountAddresses[index];
+    }
+    
+    function withdrawMoney(uint amount) isOwner returns (bool val) {
+        address sender = msg.sender;
+        val = sender.send(amount);
+    }
+    
+    function pay() {
+        
     }
 }
